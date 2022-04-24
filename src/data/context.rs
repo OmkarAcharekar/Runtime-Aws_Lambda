583709305c2fe3b4f640d4924dffdee472752d14
Omkar Acharekar <omkaracharekar12@gmail.com>
2022-04-24 20:12:20 +0530

    avoid needlessly copying data

--- src/data/context.rs
+++ src/data/context.rs
@@ -41,3 +41,81 @@ pub trait LambdaContext {
     fn cognito_identity(&self) -> Option<&str>;
     fn client_context(&self) -> Option<&str>;
 }
+
+/// A generic implementation of [`LambdaContext`] that relies on **borrowing** existing owned
+/// instances of types that implement [`crate::data::env::RuntimeEnvVars`] - for reading environment variables -
+/// and [`crate::data::response::LambdaAPIResponse`] - for reading event-related data.
+///
+/// It can be used so long that its lifetime is less than or equal to its referents.
+///
+/// This implementation is used to avoid needlessly copying data that is immutable by definition,
+/// however it is assumed that types implementing [`crate::data::response::LambdaAPIResponse`] can be read from
+/// immutably - which is not the always case with HTTP Response types,
+/// for example [ureq::Response](https://docs.rs/ureq/2.4.0/ureq/struct.Response.html#method.into_string) consumes itself upon reading the response body.
+/// See [`crate::data::response::LambdaAPIResponse`].
+pub struct RefLambdaContext<'a, E, R>
+where
+    E: RuntimeEnvVars,
+    R: LambdaAPIResponse,
+{
+    /// A shared reference to a type implementing [`crate::data::env::RuntimeEnvVars`].
+    pub env_vars: &'a E,
+    /// A shared reference to a type implementing [`crate::data::response::LambdaAPIResponse`].
+    pub invo_resp: &'a R,
+}
+
+impl<'a, E, R> LambdaContext for RefLambdaContext<'a, E, R>
+where
+    E: RuntimeEnvVars,
+    R: LambdaAPIResponse,
+{
+    #[inline]
+    fn get_deadline(&self) -> Option<Duration> {
+        self.invo_resp.deadline()
+    }
+
+    #[inline(always)]
+    fn invoked_function_arn(&self) -> Option<&str> {
+        self.invo_resp.invoked_function_arn()
+    }
+
+    #[inline(always)]
+    fn aws_request_id(&self) -> Option<&str> {
+        self.invo_resp.aws_request_id()
+    }
+
+    #[inline(always)]
+    fn function_name(&self) -> Option<&str> {
+        self.env_vars.get_function_name()
+    }
+
+    #[inline(always)]
+    fn function_version(&self) -> Option<&str> {
+        self.env_vars.get_function_version()
+    }
+
+    #[inline(always)]
+    fn memory_limit_in_mb(&self) -> Option<usize> {
+        self.env_vars.get_function_memory_size()
+    }
+
+    #[inline(always)]
+    fn log_group_name(&self) -> Option<&str> {
+        self.env_vars.get_log_group_name()
+    }
+
+    #[inline(always)]
+    fn log_stream_name(&self) -> Option<&str> {
+        self.env_vars.get_log_stream_name()
+    }
+
+    #[inline(always)]
+    fn cognito_identity(&self) -> Option<&str> {
+        self.invo_resp.cognito_identity()
+    }
+
+    #[inline(always)]
+    fn client_context(&self) -> Option<&str> {
+        self.invo_resp.client_context()
+    }
+}
